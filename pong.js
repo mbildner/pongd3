4e4983a71855681cd61b15a754cf83ae690e9b6f
Andrew Kelley <superjoe30@gmail.com>
2013-12-12 00:38:47 -0500

    we haz pong

--- pong.js
+++ pong.js
@@ -1,4 +1,192 @@
 var pong = angular.module('pong', []);
+
+pong.factory('pongService', function() {
+  window.addEventListener('keydown', onKeyDown, false);
+  window.addEventListener('keyup', onKeyUp, false);
+
+  var allGames = {};
+  var nextId = 0;
+  var upPressed = false;
+  var downPressed = false;
+
+  requestAnimationFrame(nextFrame);
+
+  function onKeyDown(event) {
+    if (event.which === 38) {
+      event.preventDefault();
+      upPressed = true;
+      return false;
+    } else if (event.which === 40) {
+      event.preventDefault();
+      downPressed = true;
+      return false;
+    }
+  }
+
+  function onKeyUp(event) {
+    if (event.which === 38) {
+      event.preventDefault();
+      upPressed = false;
+      return false;
+    } else if (event.which === 40) {
+      event.preventDefault();
+      downPressed = false;
+      return false;
+    }
+  }
+
+  function nextFrame() {
+    for (var id in allGames) {
+      var game = allGames[id]
+      game.update();
+      game.draw();
+    }
+
+    requestAnimationFrame(nextFrame);
+  }
+
+  function PongGame(canvas) {
+    this.id = nextId++;
+    this.games = [];
+
+    this.canvas = canvas;
+    this.context = canvas.getContext("2d");
+    this.onGameOver = function(){};
+
+    this.paddingX = canvas.width * 0.04;
+    this.paddingY = canvas.height * 0.04;
+    this.paddleWidth = canvas.width * 0.08;
+    this.paddleHeight = canvas.height * 0.25;
+    this.ballRadius = canvas.height * 0.08;
+    this.paddleSpeed = canvas.height * 0.02;
+
+    allGames[this.id] = this;
+
+    this.restart();
+  }
+
+  PongGame.prototype.restart = function() {
+    this.startDate = new Date();
+    this.playerY = this.canvas.height / 2 - this.paddleHeight / 2;
+    this.aiY = this.playerY;
+    this.ballPosX = this.canvas.width / 2;
+    this.ballPosY = this.canvas.height / 2;
+    var ballAngle = Math.PI * 2 * Math.random();
+    var ballSpeed = 0.7 + Math.random() * 4;
+    this.ballVelX = Math.cos(ballAngle) * ballSpeed;
+    this.ballVelY = Math.sin(ballAngle) * ballSpeed;
+
+    this.aiWantPosY = this.ballPosY;
+  };
+
+  PongGame.prototype.update = function() {
+    this.ballPosX += this.ballVelX;
+    this.ballPosY += this.ballVelY;
+
+    if (upPressed && !downPressed) {
+      this.playerY -= this.paddleSpeed;
+      if (this.playerY < 0) this.playerY = 0;
+    } else if (downPressed && !upPressed) {
+      this.playerY += this.paddleSpeed;
+      if (this.playerY + this.paddleHeight > this.canvas.height) {
+        this.playerY = this.canvas.height - this.paddleHeight;
+      }
+    }
+
+    var dist = this.aiWantPosY - (this.aiY + this.paddleHeight / 2);
+    if (dist > this.paddleSpeed * 2) {
+      this.aiY += this.paddleSpeed;
+      if (this.aiY + this.paddleHeight > this.canvas.height) {
+        this.aiY = this.canvas.height - this.paddleHeight;
+      }
+    } else if (dist < -this.paddleSpeed * 2) {
+      this.aiY -= this.paddleSpeed;
+      if (this.aiY < 0) this.aiY = 0;
+    }
+    dist = this.ballPosY - (this.aiY + this.paddleHeight / 2);
+    if (Math.abs(dist) > this.paddleHeight) {
+      this.aiWantPosY = this.ballPosY;
+    }
+
+    if (this.ballPosY - this.ballRadius < 0) {
+      this.ballVelY = Math.abs(this.ballVelY);
+    }
+    if (this.ballPosY + this.ballRadius >= this.canvas.height) {
+      this.ballVelY = -Math.abs(this.ballVelY);
+    }
+
+    if (this.ballPosX - this.ballRadius < this.paddingX + this.paddleWidth) {
+      dist = this.ballPosY - (this.playerY + this.paddleHeight / 2);
+      if (Math.abs(dist) > this.paddleHeight / 2 + this.ballRadius) {
+        this.lose();
+        return;
+      }
+      // hit player's paddle
+      this.ballVelX = Math.abs(this.ballVelX);
+    }
+    if (this.ballPosX + this.ballRadius > this.canvas.width - this.paddingX - this.paddleWidth) {
+      dist = this.ballPosY - (this.aiY + this.paddleHeight / 2);
+      if (Math.abs(dist) > this.paddleHeight / 2 + this.ballRadius) {
+        this.win();
+        return;
+      }
+      // hit ai's paddle
+      this.ballVelX = -Math.abs(this.ballVelX);
+    }
+  };
+
+  PongGame.prototype.draw = function() {
+    this.context.fillStyle = '#000000';
+    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    // paddles
+    this.context.strokeStyle = '#ffffff';
+    this.context.strokeRect(this.paddingX, this.playerY,
+        this.paddleWidth, this.paddleHeight);
+    this.context.strokeRect(this.canvas.width - this.paddingX - this.paddleWidth, this.aiY,
+        this.paddleWidth, this.paddleHeight);
+
+    // circle for ball
+    this.context.beginPath();
+    this.context.arc(this.ballPosX, this.ballPosY, this.ballRadius, 0, 2 * Math.PI);
+    this.context.closePath();
+    this.context.strokeStyle = '#ffffff';
+    this.context.stroke();
+
+  };
+
+
+  PongGame.prototype.lose = function() {
+    this.games.push({
+      win: false,
+      duration: (new Date()) - this.startDate,
+    });
+    this.onGameOver();
+    this.restart();
+  };
+
+  PongGame.prototype.win = function() {
+    this.games.push({
+      win: true,
+      duration: (new Date()) - this.startDate,
+    });
+    this.onGameOver();
+    this.restart();
+  };
+
+  return {PongGame: PongGame};
+});
+
+pong.directive('game', ['pongService', function(pongService) {
+  return {
+    template: '<canvas width="200" height="200"></canvas>',
+    link: function(scope, element, attrs) {
+      var canvas = element.children()[0];
+      var game = new pongService.PongGame(canvas);
+    },
+  };
+}]);
+
 	pong.factory('d3Service', ['$document', '$q', '$rootScope',
 		function ($document, $q, $rootScope) {
 			var deferred = $q.defer();
